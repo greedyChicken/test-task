ab502a812a5290f38851dff696c7a7260f0ac90a
Jan Hickiewicz <janhi@tutanota.com>
2023-04-05 15:40:40 +0200

    add file colors list and import components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import AddForm from "./components/AddForm";
+import ColorsList from "./components/ColorsList";
+import Container from "./components/Container";
+import FilterForm from "./components/FilterForm";
+import { colors } from "./colorsList";
+
 const App = () => {
   return (
     <div className="App">
-      
+      <Container>
+        <AddForm />
+        <FilterForm />
+        <ColorsList colors={colors} />
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/colorsList.ts
@@ -0,0 +1,1 @@
+export const colors = ["#FF0000", "#00FF00", "#0000FF"];
