3ef4c66d9059a77cf4259b91688d28d7c64c3e0c
Jan Hickiewicz <janhi@tutanota.com>
2023-04-06 14:09:43 +0200

    set colors on application start and pass proper state variables to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+import { useEffect, useState } from "react";
 import AddForm from "./components/AddForm";
 import ColorsList from "./components/ColorsList";
 import Container from "./components/Container";
 import FilterForm from "./components/FilterForm";
-import { colors } from "./colorsList";
+import useAllColors from "./useAllColors";
+import { sortColors } from "./utilities/sortColors";
 
 const App = () => {
+  const { allColors, setAllColors } = useAllColors();
+  const [colors, setColors] = useState(allColors);
+
+  useEffect(() => {
+    setColors(sortColors(allColors));
+  }, [allColors]);
+
   return (
     <div className="App">
       <Container>
-        <AddForm />
-        <FilterForm />
-        <ColorsList colors={colors} />
+        <AddForm setColors={setAllColors} />
+        <FilterForm setColors={setColors} allColors={allColors} />
+        <ColorsList
+          colors={colors}
+          setColors={setColors}
+          allColors={allColors}
+        />
       </Container>
     </div>
   );
