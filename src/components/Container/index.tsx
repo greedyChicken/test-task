a0a72551481fec977b1b2c94e2417f8e957f4708
Jan Hickiewicz <janhi@tutanota.com>
2023-04-06 23:45:52 +0200

    create and import styles for Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
+import "./style.scss";
 
 interface MyProps {
   children: ReactNode;
 }
 
 const Container = ({ children }: MyProps) => {
-  return <div>{children}</div>;
+  return <div className="container">{children}</div>;
 };
 
 export default Container;
