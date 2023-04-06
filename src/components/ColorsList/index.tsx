a6d1a743c7c1159effcf36f8f17b40b31c69fa5c
Jan Hickiewicz <janhi@tutanota.com>
2023-04-06 13:58:48 +0200

    update ColorsList component to proper displaying sorted colors

--- src/components/ColorsList/index.tsx
+++ src/components/ColorsList/index.tsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.scss";
+import { sortColors } from "../../utilities/sortColors";
 
 interface MyProps {
   colors: string[];
+  setColors: React.Dispatch<React.SetStateAction<Array<string>>>;
+  allColors: string[];
 }
 
-const ColorsList = ({ colors }: MyProps) => {
+const ColorsList = ({ colors, setColors, allColors }: MyProps) => {
+  useEffect(() => {
+    setColors(sortColors(colors));
+  }, [allColors]);
+
   return (
     <ul>
       {colors.map((color, index) => {
         return (
-          <li key={index}>
-            <div
-              className="rectangle"
-              style={{ "--my-color": color } as React.CSSProperties}
-            ></div>
-            <span>{color}</span>
-          </li>
+          color && (
+            <li key={index}>
+              <div
+                className="rectangle"
+                style={{ "--my-color": color } as React.CSSProperties}
+              ></div>
+              <span>{color}</span>
+            </li>
+          )
         );
       })}
     </ul>
