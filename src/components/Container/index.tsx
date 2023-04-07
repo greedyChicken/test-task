16c20fac0244a066c9ef6f6383af1d747dccc39e
Jan Hickiewicz <janhi@tutanota.com>
2023-04-08 01:10:05 +0200

    refactor Container to class component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,12 +1,15 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import "./style.scss";
 
 interface MyProps {
   children: ReactNode;
 }
 
-const Container = ({ children }: MyProps) => {
-  return <div className="container">{children}</div>;
-};
+class Container extends React.Component<MyProps> {
+  render() {
+    const { children } = this.props;
+    return <div className="container">{children}</div>;
+  }
+}
 
 export default Container;
